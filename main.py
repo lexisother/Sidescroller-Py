0540539df0e5ed7676ae66a6869b1cdcffc2ade1
Lexi Sother <lexisoth2005@gmail.com>
2020-02-10 20:16:35 +0000

    Updated main.py

--- main.py
+++ main.py
@@ -1,24 +1,3 @@
-"""
-Sample Python/Pygame Programs
-Simpson College Computer Science
-http://programarcadegames.com/
-http://simpson.edu/computer-science/
- 
-From:
-http://programarcadegames.com/python_examples/f.php?file=platform_scroller.py
- 
-Explanation video: http://youtu.be/QplXBw_NK5Y
- 
-Part of a series:
-http://programarcadegames.com/python_examples/f.php?file=move_with_walls_example.py
-http://programarcadegames.com/python_examples/f.php?file=maze_runner.py
-http://programarcadegames.com/python_examples/f.php?file=platform_jumper.py
-http://programarcadegames.com/python_examples/f.php?file=platform_scroller.py
-http://programarcadegames.com/python_examples/f.php?file=platform_moving.py
-http://programarcadegames.com/python_examples/sprite_sheets/
- 
-"""
- 
 import pygame
  
 # Global constants
